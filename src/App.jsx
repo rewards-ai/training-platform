7d69218c54be2f76a65907729fa1ceff4e4b9060
Pratyush-exe <pratyush21225@gmail.com>
2023-04-13 08:26:46 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import DisplayWindow from './components/DisplayWIndow/DisplayWindow'
 import Navbar from './components/Navbar/Navbar'
 import VideoStream from './components/VideoStream/VideoStream'
 import { useState } from 'react'
+import LandingPage from './LandingPage'
 
 function App() {
   const [isWin, setIsWin] = useState(0)
   return (
     <div className="App">
+      {/* <LandingPage /> */}
       {/* <VideoStream /> */}
       <Navbar isWin={isWin} setIsWin={setIsWin} />
       <DisplayWindow isWin={isWin} setIsWin={setIsWin}/>
